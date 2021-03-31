0d3cddbe5bedd42391c8a1962e38b3aaad4c0f72
Red Canary Studio <RedCanaryStudios@gmail.com>
2021-03-30 20:21:19 -0700

    Update Camera.lua

--- Camera.lua
+++ Camera.lua
@@ -1,13 +1,20 @@
 -- settings
 
 local speed = 20
+local acceleration = 5
+local maxspeed = 50
+local curspeed = 20
 local rotspeed = 0.5
+
+local turned = 0
 
 -- camera
 
 local RS = game:GetService("RunService")
 local UIS = game:GetService("UserInputService")
 local camera = workspace.CurrentCamera
+
+local moveLength = 0
 
 wait()
 
@@ -17,16 +24,41 @@ game:GetService("RunService").RenderStepped:Connect(function(dt)
     local MD = UIS:GetMouseDelta()
     
     if UIS:IsMouseButtonPressed(Enum.UserInputType.MouseButton2) then
+        local deg = math.deg
+        
         camera.CFrame = (CFrame.Angles(0, -MD.X/50*rotspeed, 0) * (camera.CFrame - camera.CFrame.Position)) + camera.CFrame.Position
-        camera.CFrame = camera.CFrame * CFrame.Angles(-MD.Y/50*rotspeed, 0, 0)
+
+        turned += math.deg(-MD.Y/50*rotspeed)
+        
+        if turned < -45 then
+            turned = -45
+        elseif turned > 90 then
+            turned = 90
+        else
+            camera.CFrame *= CFrame.Angles(-MD.Y/50*rotspeed, 0, 0)
+        end
     end
     
     local zero = Vector3.new()
     
-    camera.CFrame += (UIS:IsKeyDown(Enum.KeyCode.W) and camera.CFrame.LookVector or zero) * dt * speed
-    camera.CFrame += (UIS:IsKeyDown(Enum.KeyCode.S) and -camera.CFrame.LookVector or zero) * dt * speed
-    camera.CFrame += (UIS:IsKeyDown(Enum.KeyCode.A) and -camera.CFrame.RightVector or zero) * dt * speed
-    camera.CFrame += (UIS:IsKeyDown(Enum.KeyCode.D) and camera.CFrame.RightVector or zero) * dt * speed
+    local keys = {
+        W = UIS:IsKeyDown(Enum.KeyCode.W);
+        A = UIS:IsKeyDown(Enum.KeyCode.A);
+        S = UIS:IsKeyDown(Enum.KeyCode.S);
+        D = UIS:IsKeyDown(Enum.KeyCode.D);
+    }
+    
+    moveLength = (keys.W or keys.A or keys.S or keys.D) and moveLength + acceleration*dt or 0
+    
+    curspeed = speed + moveLength
+    
+    curspeed = math.clamp(curspeed, speed, maxspeed)
+    
+    camera.CFrame += (keys.W and camera.CFrame.LookVector or zero) * dt * curspeed
+    camera.CFrame += (keys.S and -camera.CFrame.LookVector or zero) * dt * curspeed
+    camera.CFrame += (keys.A and -camera.CFrame.RightVector or zero) * dt * curspeed
+    camera.CFrame += (keys.D and camera.CFrame.RightVector or zero) * dt * curspeed
+    
 end)
 
 UIS.InputBegan:Connect(function(input, gpe)
